c099fa08f6dd4420e79a462c670464021c9fc5ec
Jaime Bellmyer <online@bellmyer.com>
2012-10-20 09:51:54 -0500

    added base class and specs

---
+++ lib/prisoners_dilemma.rb
@@ -0,0 +1,3 @@
+class PrisonersDilemma
+  
+end
---
+++ spec/lib/prisoners_dilemma_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../spec_helper'
+require_relative '../../lib/prisoners_dilemma'
+
+describe PrisonersDilemma do
+  let(:target) { PrisonersDilemma }
+  
+  it { should be_an_instance_of(target) }
+end
