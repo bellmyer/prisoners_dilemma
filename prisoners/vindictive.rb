5f250f0ef6ef3715cf8de6fce3ddd311e642d4f7
Jaime Bellmyer <online@bellmyer.com>
2012-10-21 13:01:34 -0500

    added vindictive prisoner who never forgives

---
+++ prisoners/vindictive.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/prisoner'
+
+class Vindictive < Prisoner
+  attr_reader :betrayed
+  
+  def initialize
+    @betrayed = false
+  end
+  
+  def opponent_move= opponent_move
+    @betrayed = true if opponent_move == :betray
+  end
+  
+  def move
+    betrayed ? :betray : :cooperate
+  end
+end
