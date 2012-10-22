aff56dcc0eb7846df15b7b404dd23d4aa37fe2d7
Jaime Bellmyer <online@bellmyer.com>
2012-10-22 16:58:30 -0500

    moved sample prisoners to lib dir, so prisoners dir can be gitignored

--- helper.rb
+++ helper.rb
@@ -1,2 +1,2 @@
-Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |f| require f }
+Dir[File.dirname(__FILE__) + '/lib/**/*.rb'].each { |f| require f }
 Dir[File.dirname(__FILE__) + '/prisoners/**/*.rb'].map { |f| require(f) }
